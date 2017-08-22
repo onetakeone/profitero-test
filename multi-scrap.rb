fd5218f98452c27c76b536b4d1e9180f9b1ae2ec
N <limepassion@gmail.com>
2017-08-22 19:17:04 +0300

    get title

--- multi-scrap.rb
+++ multi-scrap.rb
@@ -3,9 +3,33 @@ require 'nokogiri'
 require 'json'
 require 'csv'
 
+### CSV data output
+def CSV_output array
+  CSV.open("csv-multi.csv", "w+") do |csv|
+    array.each do |var|
+      csv << var
+    end
+  end
+end
 
 doc = Nokogiri::HTML(open("https://www.petsonic.com/es/perros/snacks-y-huesos-perro/galletas-granja-para-perro/"))
 
+products = []
+
+doc.css('#right').each do |var|
+
+  title_t = var.at_css('h1')
+  title_t.children.each { |c| c.remove if c.name == 'span' }
+  title = title_t.text.strip
+  
+    
+    product.push(title) 
+    products << product 
+
+end
+
+puts JSON.pretty_generate(products)
+CSV_output products
 
 
 
