986c336e248e2db59079819a56bda60f8b68ba90
N <limepassion@gmail.com>
2017-08-22 19:14:30 +0300

    multi-scrap app

---
+++ multi-scrap.rb
@@ -0,0 +1,11 @@
+require 'open-uri'
+require 'nokogiri'
+require 'json'
+require 'csv'
+
+
+doc = Nokogiri::HTML(open("https://www.petsonic.com/es/perros/snacks-y-huesos-perro/galletas-granja-para-perro/"))
+
+
+
+
