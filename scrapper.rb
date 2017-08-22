56491a4877a1f466687fe112b22df45533b52c0f
N <limepassion@gmail.com>
2017-08-22 16:39:01 +0300

    get product price; output -> to function

--- scrapper.rb
+++ scrapper.rb
@@ -1,20 +1,27 @@
 require 'open-uri'
 require 'nokogiri'
 
+# Set data to file function
+def output array
+  output = File.open "scrapper.csv", "w"
+  array.each do |var|  
+    output.write " #{var},  \n"  
+  end 
+  output.close
+end
+
+# Create Nokogiri INSTANCE
 doc = Nokogiri::HTML(open("https://www.petsonic.com/es/perros/snacks-y-huesos-perro/"))
 
 products = []
 
+
 doc.css('.top-product-meta').each do |showing|
   title = showing.at_css('.product-name').text.strip
-  products.push(title,)
+  price_1 = showing.at_css('.product-price')
+  price =  price_1.text.strip.delete "desde  "
+  products.push(" #{title}, #{price}")
 end
 
+output products
 
-output = File.open "scrapper.csv", "w"
-
-products.each do |var|  
-    output.write " #{var},  \n"  
-end 
-
-output.close
