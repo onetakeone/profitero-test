e3cb957a63bacd228b7e45178b6b5b715d74d85e
N <limepassion@gmail.com>
2017-08-22 16:15:01 +0300

    get product name; put name to file

--- scrapper.rb
+++ scrapper.rb
@@ -1,3 +1,20 @@
 require 'open-uri'
+require 'nokogiri'
 
-doc = Nokogiri::HTML(open("http://www.petsonic.com/es/perros/snacks-y-huesos-perro"))
+doc = Nokogiri::HTML(open("https://www.petsonic.com/es/perros/snacks-y-huesos-perro/"))
+
+products = []
+
+doc.css('.top-product-meta').each do |showing|
+  title = showing.at_css('.product-name').text.strip
+  products.push(title,)
+end
+
+
+output = File.open "scrapper.csv", "w"
+
+products.each do |var|  
+    output.write " #{var},  \n"  
+end 
+
+output.close
