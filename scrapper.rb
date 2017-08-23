d66fdb3a097807a7e5a37d75296962c116eacdc6
N <limepassion@gmail.com>
2017-08-23 12:19:58 +0300

    ++ comments

--- scrapper.rb
+++ scrapper.rb
@@ -29,46 +29,47 @@ doc.css('.product-container').each do |var|
   price = var.at_css('.product-price').text.strip.delete "desde  "
   image = var.at_css('.product_img_link img').attr('src')
 
-  ################################################
+  ### Scraps each product item from maing page
+  #############################################<
   link = var.at_css('.product-name')['href']
   product_page = Nokogiri::HTML(open(link))
   product_page.css('#right').each do |var1|
 
+    # Get title of product
     title_t = var1.at_css('h1')
+    # Cut off <span> elements
     title_t.children.each { |c| c.remove if c.name == 'span' }
     title = title_t.text.strip
     
+    # Create multiple prices array 
     prices = []
     var1.css('.attribute_labels_lists .attribute_price').each do |p|
       prices.push(p.text.strip)
     end
-      
+    
+    # Create multiple weights array   
     weights = []
     var1.css(".attribute_labels_lists .attribute_name").each do |w|
       weights.push(w.text.strip)
     end
     
+    # Create complete array of product items (title, weight, price)
     weights.length.times do |i|
       prod = []
       prod.push( title: title, weight: weights[i], price: prices[i]) 
       single_product << prod
     end    
-  end
-
+  end  
   #puts JSON.pretty_generate(single_product)
-  # CSV_output products
-  ################################################
+  #############################################<
 
   product.push(title, price, image, link, single_product)
   products.push(product)
-  #puts JSON.pretty_generate(products)
 end
 
-
-
 ### Console data output 
-#puts JSON.pretty_generate(single_product)
-#puts JSON.pretty_generate(products)
+
+puts JSON.pretty_generate(products)
 
 ### Writes output data to csv file
 CSV_output products
