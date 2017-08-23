8f1741953ecbbb8b3bc569b6323c05951506cd77
N <limepassion@gmail.com>
2017-08-23 12:13:26 +0300

    app grabs each product by link from main page

--- scrapper.rb
+++ scrapper.rb
@@ -22,48 +22,52 @@ single_product = []
 
 ### Scrapping 
 doc.css('.product-container').each do |var|
+  single_product = []
   product = []
+
   title = var.at_css('.product-name').text.strip
   price = var.at_css('.product-price').text.strip.delete "desde  "
   image = var.at_css('.product_img_link img').attr('src')
 
-    ################################################
-    link = var.at_css('.product-name')['href']
-    product_page = Nokogiri::HTML(open(link))
-    
+  ################################################
+  link = var.at_css('.product-name')['href']
+  product_page = Nokogiri::HTML(open(link))
+  product_page.css('#right').each do |var1|
 
-    product_page.css('#right').each do |var|
-      title_t = var.at_css('h1')
-      title_t.children.each { |c| c.remove if c.name == 'span' }
-      title = title_t.text.strip
+    title_t = var1.at_css('h1')
+    title_t.children.each { |c| c.remove if c.name == 'span' }
+    title = title_t.text.strip
+    
+    prices = []
+    var1.css('.attribute_labels_lists .attribute_price').each do |p|
+      prices.push(p.text.strip)
+    end
       
-      prices = []
-      var.css('.attribute_labels_lists .attribute_price').each do |p|
-        prices.push(p.text.strip)
-      end
-        
-      weights = []
-      var.css(".attribute_labels_lists .attribute_name").each do |w|
-        weights.push(w.text.strip)
-      end
-      
-      weights.length.times do |i|
-        prod = []
-        product.push(title, weights[i], prices[i]) 
-        single_product << prod
-      end    
+    weights = []
+    var1.css(".attribute_labels_lists .attribute_name").each do |w|
+      weights.push(w.text.strip)
     end
     
-    # CSV_output products
-    ################################################
+    weights.length.times do |i|
+      prod = []
+      prod.push( title: title, weight: weights[i], price: prices[i]) 
+      single_product << prod
+    end    
+  end
+
+  puts JSON.pretty_generate(single_product)
+  # CSV_output products
+  ################################################
 
   product.push(title, price, image, link)
   products.push(product)
 end
 
+
+
 ### Console data output 
-puts JSON.pretty_generate(single_product)
-puts JSON.pretty_generate(products)
+#puts JSON.pretty_generate(single_product)
+#puts JSON.pretty_generate(products)
 
 ### Writes output data to csv file
 # CSV_output products
