453be677d575bf854824e7396868f4aeab100f4b
N <limepassion@gmail.com>
2017-08-24 14:38:37 +0300

    puts each product variety in line

--- scrapper.rb
+++ scrapper.rb
@@ -1,9 +1,10 @@
 require 'open-uri'
 require 'nokogiri'
 require 'csv'
+require 'json'
 
 def output array
-  CSV.open("csv.csv", "w+") do |csv|
+  CSV.open("csv-test.csv", "w+") do |csv|
     array.each do |var|
       csv << var
     end
@@ -13,17 +14,16 @@ end
 csv = []
 1.upto(12) do |i|
   page = Nokogiri::HTML(open("https://www.petsonic.com/snacks-huesos-para-perros/?p=#{i}"))  
-  page.xpath('//*[@id="center_column"]/div/div/div/div/div/div/div/div/h5/a').each do |var|   
+  page.xpath('//*[@id="center_column"]/div/div/div/div/div/div/div/div/h5/a').each do |var|  
     product = Nokogiri::HTML(open(var['href']))
     title = product.xpath('//*[@id="right"]/div/div[1]/div/h1/text()')
     image = product.xpath('//*[@id="bigpic"]').attr('src')    
-    product_info = []
-    product_info.push(title, image)
-    product.xpath('//*[@id="attributes"]/fieldset/div/ul[@class="attribute_labels_lists"]/li/span[position() <= 2]/text()').each do |weight_price|
-      product_info.push(weight_price)
-    end
-    csv << product_info
-  end  
+    product.xpath('//*[@id="attributes"]/fieldset/div/ul[@class="attribute_labels_lists"]').each_with_index do |lab, i|
+      product_info = []  
+      product_info.push( title, image, lab.xpath('//li/span[@class="attribute_name"]/text()')[i], lab.xpath('//li/span[@class="attribute_price"]/text()')[i] )
+      csv.push(hh)      
+    end             
+  end 
 end
 output csv
 
